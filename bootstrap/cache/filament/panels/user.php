8ba4ccb6888687618cb2a007be0368bd4c3012ce
AhmadFrazDEV <chahmad2004@gmail.com>
2025-09-13 03:32:46 -0700

    Filament - Dashboard

---
+++ bootstrap/cache/filament/panels/user.php
@@ -0,0 +1,62 @@
+<?php return array (
+  'livewireComponents' => 
+  array (
+    'filament.pages.dashboard' => 'Filament\\Pages\\Dashboard',
+    'filament.widgets.account-widget' => 'Filament\\Widgets\\AccountWidget',
+    'filament.livewire.database-notifications' => 'Filament\\Livewire\\DatabaseNotifications',
+    'filament.auth.pages.edit-profile' => 'Filament\\Auth\\Pages\\EditProfile',
+    'filament.livewire.global-search' => 'Filament\\Livewire\\GlobalSearch',
+    'filament.livewire.notifications' => 'Filament\\Livewire\\Notifications',
+    'filament.livewire.sidebar' => 'Filament\\Livewire\\Sidebar',
+    'filament.livewire.simple-user-menu' => 'Filament\\Livewire\\SimpleUserMenu',
+    'filament.livewire.topbar' => 'Filament\\Livewire\\Topbar',
+    'filament.auth.pages.login' => 'Filament\\Auth\\Pages\\Login',
+  ),
+  'clusters' => 
+  array (
+  ),
+  'clusteredComponents' => 
+  array (
+  ),
+  'clusterDirectories' => 
+  array (
+  ),
+  'clusterNamespaces' => 
+  array (
+  ),
+  'pages' => 
+  array (
+    0 => 'Filament\\Pages\\Dashboard',
+  ),
+  'pageDirectories' => 
+  array (
+    0 => 'C:\\Clinic App\\doc\\app\\Filament/Pages',
+  ),
+  'pageNamespaces' => 
+  array (
+    0 => 'App\\Filament\\Pages',
+  ),
+  'resources' => 
+  array (
+  ),
+  'resourceDirectories' => 
+  array (
+    0 => 'C:\\Clinic App\\doc\\app\\Filament/Resources',
+  ),
+  'resourceNamespaces' => 
+  array (
+    0 => 'App\\Filament\\Resources',
+  ),
+  'widgets' => 
+  array (
+    0 => 'Filament\\Widgets\\AccountWidget',
+  ),
+  'widgetDirectories' => 
+  array (
+    0 => 'C:\\Clinic App\\doc\\app\\Filament/Widgets',
+  ),
+  'widgetNamespaces' => 
+  array (
+    0 => 'App\\Filament\\Widgets',
+  ),
+);
